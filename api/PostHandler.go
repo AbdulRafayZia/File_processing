d7a9838340b3578c0a0ab00ce50e2a19f4eaf886
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-11-19 01:15:44 +0500

    implement file upload but failed conflict occcur

--- api/PostHandler.go
+++ api/PostHandler.go
@@ -2,8 +2,14 @@ package api
 
 import (
 	"encoding/json"
-	"net/http"
+	"fmt"
 	"gorilla/pkg"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"time"
+	"os"
+	"io"
 )
 
 // RequestBody is the structure for the incoming JSON payload
@@ -13,14 +19,14 @@ type RequestBody struct {
 
 // ResponseBody is the structure for the outgoing JSON response
 type ResponseBody struct {
-	TotalLines int `json:"TotalLines"`
-	TotalWords int `json:"TotalWords"`
+	TotalLines       int `json:"TotalLines"`
+	TotalWords       int `json:"TotalWords"`
 	TotalPuncuations int `json:"TotalPuncuations"`
-	TotalVowels int `json:"TotalVowels"`
-
-
+	TotalVowels      int `json:"TotalVowels"`
 }
+
 func HandlePostRequest(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
 	var requestBody RequestBody
 
 	// Decode the JSON payload
@@ -31,14 +37,43 @@ func HandlePostRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		fmt.Println("error while getting the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	// Create a new file in the server's upload directory
+	filePath := filepath.Join("uploads", handler.Filename)
+	dst, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "Unable to create the file for writing", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+		// Copy the file content to the new file
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			http.Error(w, "Unable to write the file", http.StatusInternalServerError)
+			return
+		}
+	fileBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileData := string(fileBytes)
+
 	// Call YourFunction with the provided value
-	result := pkg.ProcessFile(requestBody.Routines)
+	result := pkg.ProcessFile(requestBody.Routines , fileData)
 
 	// Create the response payload
 	responseBody := ResponseBody{
-		TotalLines : result.LineCount,
-		TotalWords:result.WordsCount ,
-		TotalVowels: result.VowelsCount,
+		TotalLines:       result.LineCount,
+		TotalWords:       result.WordsCount,
+		TotalVowels:      result.VowelsCount,
 		TotalPuncuations: result.PuncuationsCount,
 	}
 
@@ -46,4 +81,9 @@ func HandlePostRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(responseBody)
+	endTime := time.Now()
+
+	// Calculate the execution time
+	executionTime := endTime.Sub(startTime)
+	fmt.Printf("Execution time: %v\n", executionTime)
 }
--- pkg/processFile.go
+++ pkg/processFile.go
@@ -1,36 +1,58 @@
 package pkg
 
 import (
-	"io/ioutil"
-	"log"
+	// "io/ioutil"
+	
+	"sync"
 )
-func ProcessFile( routines int) Summary {
+func ProcessFile( Routines int, FileData string) Summary {
+	var summary Summary
+	var wg sync.WaitGroup
     channal := make(chan Summary)
-    content, err := ioutil.ReadFile("D:/gorilla/assests/file.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fileData := string(content)
-    chunk := len(fileData) / routines
+    // content, err := ioutil.ReadFile("D:/gorilla/assests/file.txt")
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // FileData := string(content)
+    chunk := len(FileData) / Routines
     startIndex := 0
     endIndex := chunk
-    for iterations := 0; iterations < routines; iterations++ {
-        go Counts(fileData[startIndex:endIndex], channal)
+    for iterations := 0; iterations < Routines; iterations++ {
+		wg.Add(1)
+		go func(start, end int) {
+			defer wg.Done()
+			Counts(FileData[start:end], channal)
+		}(startIndex, endIndex)
+        // go Counts(fileData[startIndex:endIndex], channal)
         startIndex = endIndex
         endIndex += chunk
+      
     }
-	var summary Summary
-    for iterations := 0; iterations < routines; iterations++ {
-        counts := <-channal
-        // fmt.Printf("number of lines of chunk %d: %d \n", iterations+1, counts.LineCount)
-        // fmt.Printf("number of words of chunk %d: %d \n", iterations+1, counts.WordsCount)
-        // fmt.Printf("number of vowels of chunk %d: %d \n", iterations+1, counts.VowelsCount)
-        // fmt.Printf("number of puncuations of chunk %d: %d \n", iterations+1, counts.PuncuationsCount)
-		summary.LineCount+=counts.LineCount;
-		summary.WordsCount+=counts.WordsCount;
-		summary.VowelsCount+=counts.VowelsCount;
-		summary.PuncuationsCount+=counts.PuncuationsCount;
+	// Close the channel once all goroutines are done
+	go func() {
+		wg.Wait()
+		close(channal)
+	}()
 
-    }
+	// Receive results from the channel and aggregate them
+	for counts := range channal {
+		summary.LineCount += counts.LineCount
+		summary.WordsCount += counts.WordsCount
+		summary.VowelsCount += counts.VowelsCount
+		summary.PuncuationsCount += counts.PuncuationsCount
+	}
+	
+    // for iterations := 0; iterations < routines; iterations++ {
+    //     counts := <-channal
+    //     // fmt.Printf("number of lines of chunk %d: %d \n", iterations+1, counts.LineCount)
+    //     // fmt.Printf("number of words of chunk %d: %d \n", iterations+1, counts.WordsCount)
+    //     // fmt.Printf("number of vowels of chunk %d: %d \n", iterations+1, counts.VowelsCount)
+    //     // fmt.Printf("number of puncuations of chunk %d: %d \n", iterations+1, counts.PuncuationsCount)
+	// 	summary.LineCount+=counts.LineCount;
+	// 	summary.WordsCount+=counts.WordsCount;
+	// 	summary.VowelsCount+=counts.VowelsCount;
+	// 	summary.PuncuationsCount+=counts.PuncuationsCount;
+
+    // }
 	return summary
 }
