6da7471536405f61abf76456ba7f70fba2a0dfe0
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-11-22 15:23:52 +0500

    dome

--- main.go
+++ main.go
@@ -15,10 +15,7 @@ import (
 func main() {
 	
 	r := mux.NewRouter()
-
-	// Define the route for the POST request
-	// r.HandleFunc("/api/FileSummary", api.HandlePostRequest).Methods("POST")
-	r.HandleFunc("/login", login.LoginHandler).Methods("GET")
+	r.HandleFunc("/login", login.LoginHandler).Methods("POST")
 	r.HandleFunc("/protected", login.ProtectedHandler).Methods("POST")
 	// Start the server
 	port := 8080
