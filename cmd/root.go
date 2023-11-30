010c5828d6042fb8cd6415ee4d6b96a1bbda84c4
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-11-30 17:08:58 +0500

    final

--- cmd/root.go
+++ cmd/root.go
@@ -11,11 +11,18 @@ import (
 )
 
 func Execute() {
+	r := routes.Routes()
+	port := 8080
+	utils.Rafay()
+
+	fmt.Printf("Server listening on :%d...\n", port)
+
+	// Start the server
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r))
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 
 }
-
-
-
-
-
-
--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/AbdulRafayZia/Gorilla-mux/internal/infrastructure/routes"
-	"github.com/AbdulRafayZia/Gorilla-mux/utils"
-	"github.com/gorilla/handlers"
+	"github.com/AbdulRafayZia/Gorilla-mux/cmd"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-
-	r := routes.Routes()
-	port := 8080
-	utils.Rafay()
-
-	fmt.Printf("Server listening on :%d...\n", port)
-
-	// Start the server
-	headersOk := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
-	originsOk := handlers.AllowedOrigins([]string{"*"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r))
-
-	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	cmd.Execute()
 
 }
